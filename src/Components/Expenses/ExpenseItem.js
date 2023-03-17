2e18507b2f030b1d0395f26c6718d047fa593b6e
davidjhaa4u <davidjhaa4u@gmail.com>
2023-03-17 14:13:10 +0530

    Dynamic behaviour started

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
+    const[newTitle, setNewTitle] = useState("");
+    const [title, setTitle] = useState(props.title);
+
+    const changeHandler = (event) => {
+        setNewTitle(event.target.value);
+    }
+
+    const clickHandler = () => {
+        setTitle(newTitle);
+    }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{ props.title }</h2>
+                <h2>{ title }</h2>
                 <div className='expense-item__price'>${ props.amount}</div> 
             </div>
+            <input type="text" value={newTitle} onChange={changeHandler}/>
+            <button onClick={ clickHandler }>Change Title</button>
         </Card>
     );
 }
